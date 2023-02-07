2475df8babb1f01a962afffee0ef42de9594c9b9
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2023-02-07 20:23:39 +0530

    Test data added

--- demo/src/main/java/com/springproject/demo/DemoApplication.java
+++ demo/src/main/java/com/springproject/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.springproject.demo;
 
+import com.springproject.demo.model.User;
+import com.springproject.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("Sandun", "Rangana", "sandun@gmail.com"));
+		this.userRepository.save(new User("kasun", "jaye", "sandun88@gmail.com"));
+		this.userRepository.save(new User("nimal", "darshana", "sandusdfn@gmail.com"));
+		this.userRepository.save(new User("kasy", "panditha", "sanddfsun@gmail.com"));
+	}
 }
--- demo/src/main/java/com/springproject/demo/controller/UserController.java
+++ demo/src/main/java/com/springproject/demo/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.springproject.demo.controller;
 
+import com.springproject.demo.model.User;
+import com.springproject.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("users")
+    public List<User> getUsers(){
+        return this.userRepository.findAll();
+    }
+
 }
